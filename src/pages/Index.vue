6965ee4fc5971dd3a8bc5cdd58d597525efd9e77
Atria <atriasft@gmail.com>
2020-11-07 02:44:01 +0900

    これでオッケー

---
+++ src/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page>
+    <h4>Main</h4>
+    <p>This is main page :)</p>
+    <q-btn
+      @click="HelloWorld"
+      color="primary"
+      label="Showing norify."
+    />
+  </q-page>
+</template>
+
+<script>
+export default {
+  methods: {
+    HelloWorld () {
+      this.$q.notify('Hello,World!')
+    }
+  }
+}
+</script>
